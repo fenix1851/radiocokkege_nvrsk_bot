9c53a27bb6c6e7c374ca9816284a023c37744e74
Pyatiletov Dmitriy <foxyess@mail.ru>
2022-10-06 10:45:35 +0300

    a

--- src/services/sheduleParse.js
+++ src/services/sheduleParse.js
@@ -5,7 +5,9 @@ async function main(){
     const html = await fsp.readFile('./src/data/shedule.html')
     const $ = cherio.load(html)
     const tables = $('table[class=MsoNormalTable]')
-    console.log(tables)
+    table = $(tables[0])
+    tds = $('td')
+    console.log(tds[2].text())
     const groups = {}
     // for(let table in tables){
     //     table = $(table)
