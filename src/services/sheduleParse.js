63bceff2c41236c19b4b99055b5ed544bdc37e0a
Pyatiletov Dmitriy <foxyess@mail.ru>
2022-10-06 10:13:10 +0300

    adding simple parsing for table

--- src/services/sheduleParse.js
+++ src/services/sheduleParse.js
@@ -1,1 +1,9 @@
 const cherio = require('cherio');
+const fsp = require('fs').promises
+
+async function main(){
+    const html = await fsp.readFile('./src/data/shedule.html')
+    const $ = cherio.load(html)
+    const tables = $('table[class=MsoNormalTable]')
+    console.log(tables)
+}
