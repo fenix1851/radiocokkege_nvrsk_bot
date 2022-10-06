a646412f248bbc2704a4b49f86bc63c3f403446d
Pyatiletov Dmitriy <foxyess@mail.ru>
2022-10-06 10:27:16 +0300

    again

--- src/services/sheduleParse.js
+++ src/services/sheduleParse.js
@@ -9,7 +9,7 @@ async function main(){
     const groups = {}
     for(let table in tables){
         table = cherio.load(table)
-        const firstRow = table('tr')[0]
+        const firstRow = cherio.load(table('tr')[0])
         const groupsColumns = firstRow('td')
         for(let column in groupsColumns){
             column = cherio.load(column)
