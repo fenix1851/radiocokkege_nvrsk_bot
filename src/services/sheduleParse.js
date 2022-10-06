4d007b5fef233aa7fd75f1af152810920d2c7ab4
Pyatiletov Dmitriy <foxyess@mail.ru>
2022-10-06 11:04:46 +0300

    commit

--- src/services/sheduleParse.js
+++ src/services/sheduleParse.js
@@ -5,19 +5,9 @@ async function main(){
     const html = await fsp.readFile('./src/data/shedule.html')
     const $ = cherio.load(html)
     const tables = $('table[class=MsoNormalTable]')
-    table = $(tables[0])
-    tds = table.children('td')
-    console.log(tds)
-    const groups = {}
-    // for(let table in tables){
-    //     table = $(table)
-    //     firstRow = 
-    //     const groupsColumns = firstRow('td')
-    //     for(let column in groupsColumns){
-    //         column = cherio.load(column)
-    //         groups[column.text()] = {}
-    //     }
-    // }
+    tables.each((idx,table)=>{
+        const tds = $(table).find('td')
+    })
     console.log(groups)
 }
 
