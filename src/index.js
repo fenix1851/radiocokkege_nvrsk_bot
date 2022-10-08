bc9ea46e6e40ffdde7c7be6adad731298f9ddb05
Pyatiletov Dmitriy <foxyess@mail.ru>
2022-10-08 11:54:02 +0300

    added .env

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-const parser = require('./src/services/sheduleParser.js')
+process
+ .on('unhandledRejection', (reason, p) => {
+    console.log(`Unhandled Rejection at Promise: ${reason}`);
+ })
+ .on('uncaughtException', err => {
+    console.log(`Uncaught Exception thrown: ${err}`);
+ });
 
-parser.main()
+require('dotenv').config();
+
+const { BOT_TOKEN, TEST_TOKEN, MONGODB_URI, ADMIN_ID } = process.env;
+const {Telegraf, Scenes: {Stage}} = require('telegraf')
+
+const {MongoClient} = require('mongodb')
+const {session} = require('telegraf-session-mongodb')
+
+const fs = require('fs')
+const fsp = fs.promises;
