e1dec0abff3f083639ac1a6aa900d59347a004f7
Pyatiletov Dmitriy <foxyess@mail.ru>
2022-10-09 20:13:05 +0300

    Add files via upload

--- src/keyboards/groups.js
+++ src/keyboards/groups.js
@@ -1,13 +1,242 @@
 const groupKeyboard = {
     inline_keyboard: [
-    [
-        {text: '1-О-1',callback_data: '1-О-1'},
-        {text: '1-О-2', callback_data: '1-О-2'},
-        {text: '1-ОТ-1', callback_data: '1-ОТ-1'},
-        {text: '1-ОТ-2.', callback_data: '1-ОТ-2'},
-        {text: '1-ОТ-3.', callback_data: '1-ОТ-3'},
-    ]
-]
+        
+            [{
+                    text: '1-О-1',
+                    callback_data: '1-О-1'
+                },
+                {
+                    text: '1-О-2',
+                    callback_data: '1-О-2'
+                },
+                {
+                    text: '1-ОТ-1',
+                    callback_data: '1-ОТ-1'
+                },
+                {
+                    text: '1-ОТ-2',
+                    callback_data: '1-ОТ-2'
+                },
+                {
+                    text: '1-ОТ-3',
+                    callback_data: '1-ОТ-3'
+                },
+                {
+                    text: '1-ОТ-4',
+                    callback_data: '1-ОТ-4'
+                }
+            ],
+            [{
+                    text: '1-Р-1',
+                    callback_data: '1-Р-1'
+                },
+                {
+                    text: '1-Р-2',
+                    callback_data: '1-Р-2'
+                },
+                {
+                    text: '1-ТВ-1',
+                    callback_data: '1-ТВ-1'
+                },
+                {
+                    text: '1-ТВ-2',
+                    callback_data: '1-ТВ-2'
+                },
+                {
+                    text: '1-ТЭ-1',
+                    callback_data: '1-ТЭ-1'
+                },
+                {
+                    text: '1-ТЭ-2',
+                    callback_data: '1-ТЭ-2'
+                }
+            ],
+            [{
+                    text: '1-Ф-1',
+                    callback_data: '1-Ф-1'
+                },
+                {
+                    text: '1-Ф-2',
+                    callback_data: '1-Ф-2'
+                },
+                {
+                    text: '2-О-1',
+                    callback_data: '2-О-1'
+                },
+                {
+                    text: '2-ОТ-1',
+                    callback_data: '2-ОТ-1'
+                },
+                {
+                    text: '2-ОТ-2',
+                    callback_data: '2-ОТ-2'
+                },
+                {
+                    text: '2-ОТ-3',
+                    callback_data: '2-ОТ-3'
+                }
+            ],
+            [{
+                    text: '2-ОТ-4',
+                    callback_data: '2-ОТ-4'
+                },
+                {
+                    text: '2-Р-1',
+                    callback_data: '2-Р-1'
+                },
+                {
+                    text: '2-Р-2',
+                    callback_data: '2-Р-2'
+                },
+                {
+                    text: '2-ТВ-1',
+                    callback_data: '2-ТВ-1'
+                },
+                {
+                    text: '2-ТВ-2',
+                    callback_data: '2-ТВ-2'
+                },
+                {
+                    text: '2-ТЭ-1',
+                    callback_data: '2-ТЭ-1'
+                }
+            ],
+            [{
+                    text: '2-ТЭ-2',
+                    callback_data: '2-ТЭ-2'
+                },
+                {
+                    text: '2-ТЭ-3',
+                    callback_data: '2-ТЭ-3'
+                },
+                {
+                    text: '2-Ф-1',
+                    callback_data: '2-Ф-1'
+                },
+                {
+                    text: '2-Ф-2',
+                    callback_data: '2-Ф-2'
+                },
+                {
+                    text: '2-Ф-3',
+                    callback_data: '2-Ф-3'
+                },
+                {
+                    text: '3-О-1',
+                    callback_data: '3-О-1'
+                }
+            ],
+            [{
+                    text: '3-ОТ-1',
+                    callback_data: '3-ОТ-1'
+                },
+                {
+                    text: '3-ОТ-2',
+                    callback_data: '3-ОТ-2'
+                },
+                {
+                    text: '3-П-1',
+                    callback_data: '3-П-1'
+                },
+                {
+                    text: '3-П-2',
+                    callback_data: '3-П-2'
+                },
+                {
+                    text: '3-М-1',
+                    callback_data: '3-М-1'
+                },
+                {
+                    text: '3-М-2',
+                    callback_data: '3-М-2'
+                }
+            ],
+            [{
+                    text: '3-Р-1',
+                    callback_data: '3-Р-1'
+                },
+                {
+                    text: '3-Р-2',
+                    callback_data: '3-Р-2'
+                },
+                {
+                    text: '3-ТВ-1',
+                    callback_data: '3-ТВ-1'
+                },
+                {
+                    text: '3-ТВ-2',
+                    callback_data: '3-ТВ-2'
+                },
+                {
+                    text: '3-ТЭ-1',
+                    callback_data: '3-ТЭ-1'
+                },
+                {
+                    text: '3-Ф-1',
+                    callback_data: '3-Ф-1'
+                }
+            ],
+            [{
+                    text: '3-Ф-2',
+                    callback_data: '3-Ф-2'
+                },
+                {
+                    text: '4-А-1',
+                    callback_data: '4-А-1'
+                },
+                {
+                    text: '4-М-1',
+                    callback_data: '4-М-1'
+                },
+                {
+                    text: '4-М-2',
+                    callback_data: '4-М-2'
+                },
+                {
+                    text: '4-М-3',
+                    callback_data: '4-М-3'
+                },
+                {
+                    text: '4-О-1',
+                    callback_data: '4-О-1'
+                }
+            ],
+            [{
+                    text: '4-ОТ-1',
+                    callback_data: '4-ОТ-1'
+                },
+                {
+                    text: '4-ОТ-2',
+                    callback_data: '4-ОТ-2'
+                },
+                {
+                    text: '4-П-1',
+                    callback_data: '4-П-1'
+                },
+                {
+                    text: '4-П-2',
+                    callback_data: '4-П-2'
+                },
+                {
+                    text: '4-Р-1',
+                    callback_data: '4-Р-1'
+                },
+                {
+                    text: '4-Т-1',
+                    callback_data: '4-Т-1'
+                }
+            ],
+            [{
+                    text: '4-ТЭ-1',
+                    callback_data: '4-ТЭ-1'
+                },
+                {
+                    text: '5-О-1',
+                    callback_data: '5-О-1'
+                }
+            ],
+        ]
+    
 }
 
 module.exports = groupKeyboard
